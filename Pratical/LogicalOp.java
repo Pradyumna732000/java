a32f435b2ddc464099f214d218a8165f48e46553
Pradyumna732000 <105195390+Pradyumna732000@users.noreply.github.com>
2022-05-18 12:52:38 +0530

    Add files via upload

---
+++ Pratical/LogicalOp.java
@@ -0,0 +1,31 @@
+package corejava;
+
+public class LogicalOp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// intialising variables
+		int a=10,b=20,c=20,d=0;
+		
+		//a,b,c displaying
+		System.out.println("Value of a:"+a);
+		System.out.println("Value of b:"+b);
+		System.out.println("Value of c:"+c);
+		//using logical AND to Verify Two constraints
+		if(a>b ||c==d)
+			System.out.println("One or both"+"the conditions are true");
+		else
+			System.out.println(" Both the"+"conditions are false");
+	}
+
+			
+
+
+				
+			
+
+		
+
+	}
+
+

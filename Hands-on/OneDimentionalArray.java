b6d7d394e423b5796f194f953108deb6e78c4211
Pradyumna732000 <105195390+Pradyumna732000@users.noreply.github.com>
2022-05-23 12:27:59 +0530

    Add files via upload

---
+++ Hands-on/OneDimentionalArray.java
@@ -0,0 +1,27 @@
+package corejava;
+
+public class OneDimentionalArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+				String a[]=new String[5];
+				
+				a[0]="Pradyumna";
+				//initialization
+				a[1]="Suraj";
+				a[2]="Amit";
+				a[3]="Pawan"; 
+				a[4]="Shubham"; 
+				
+				//traversing array 
+						for(int i=0;i<a.length;i++)
+							//length is the property of array 
+							System.out.println(a[i]); 
+						}		
+
+			
+
+	}
+
+
